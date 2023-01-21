cbf40096b0109832854f121360715eb5b58893e2
Pet2Ant <niotisan@gmail.com>
2023-01-22 00:06:52 +0200

    Added contact page, loader, survey pop-up to fulfill project requirements

--- public/main.js
+++ public/main.js
@@ -29,25 +29,10 @@ function topFunction() {
     
     }
 
-//changeToImageUpload function that changes from textArea to Image upload using classList
-function changeToImageUpload() {
-    document.getElementById("textArea").classList.add("hidden");
-    document.getElementById("imageUpload").classList.remove("hidden");
-    // Change the background color and text color of the imageButton to the colors of the TextButton using classList
-    document.getElementById("imageButton").classList.add("bg-[#ff4057]"); 
-    document.getElementById("imageButton").classList.remove("bg-zinc-800");
-    document.getElementById("imageButton").classList.add("text-white");
-    document.getElementById("imageButton").classList.remove("text-[#ff4057]");
-    document.getElementById("imageButton").classList.remove("hover:text-[#ff4957]");
-    document.getElementById("textButton").classList.add("bg-zinc-800");
-    document.getElementById("textButton").classList.remove("bg-[#ff4057]");
-    document.getElementById("textButton").classList.add("text-[#ff4057]");
-    document.getElementById("textButton").classList.remove("text-white");
-    document.getElementById("textButton").classList.add("hover:text-white");
-}
 
-//changeToTextArea function that changes from Image upload to textArea using classList
-function changeToTextArea() {
+// function to change the button between text and image in the createPost page
+function changeButton() {
+  if (document.getElementById("textArea").classList.contains("hidden")) {
     document.getElementById("imageUpload").classList.add("hidden");
     document.getElementById("textArea").classList.remove("hidden");
     // Change the background color and text color of the textButton to the colors of the imageButton using classList
@@ -62,4 +47,53 @@ function changeToTextArea() {
     document.getElementById("imageButton").classList.remove("text-white");
     document.getElementById("imageButton").classList.add("hover:text-white");
     document.getElementById("imageButton").classList.add("hover:bg-[#ff4957]");
+  } else {
+    document.getElementById("textArea").classList.add("hidden");
+    document.getElementById("imageUpload").classList.remove("hidden");
+    // Change the background color and text color of the imageButton to the colors of the TextButton using classList
+    document.getElementById("imageButton").classList.add("bg-[#ff4057]"); 
+    document.getElementById("imageButton").classList.remove("bg-zinc-800");
+    document.getElementById("imageButton").classList.add("text-white");
+    document.getElementById("imageButton").classList.remove("text-[#ff4057]");
+    document.getElementById("imageButton").classList.remove("hover:text-[#ff4957]");
+    document.getElementById("textButton").classList.add("bg-zinc-800");
+    document.getElementById("textButton").classList.remove("bg-[#ff4057]");
+    document.getElementById("textButton").classList.add("text-[#ff4057]");
+    document.getElementById("textButton").classList.remove("text-white");
+    document.getElementById("textButton").classList.add("hover:text-white");
+  }
+}
+
+// Loader function that transitions smoothly from loader to page
+document.onreadystatechange = function() {
+    if (document.readyState !== "complete") {
+      document.querySelector("body").style.visibility = "hidden";
+      document.querySelector("#loader").style.visibility = "visible";
+    } else {
+      // Make the body visible again with a smooth transition
+      document.querySelector("#loader").style.display = "none";
+      document.querySelector("body").style.visibility = "visible";
+    }
+
+// Open pop-up window with id of "survey-popup" when the user stays on the page for 5 minutes
+setTimeout(function() {
+  document.getElementById("survey-popup").classList.remove("hidden");
+  document.getElementById("survey-popup").classList.add("flex");
+  document.getElementById("survey-popup").classList.add("justify-center");
+// add to body the class of "overflow-hidden" to prevent scrolling when the pop-up window is open
+  document.querySelector("body").classList.add("overflow-hidden");
+  document.querySelector("header").classList.add("hidden");
+  // automatically bring user to top of page when pop-up window opens
+  window.scrollTo(0, 0);
+}, 3000);
+}
+
+// Close pop-up window with id of "survey-popup" when the user clicks on the "X" button, then add to the id of the pop-up window the class of "hidden"
+function closeSurvey() {
+  document.getElementById("survey-popup").classList.add("hidden");
+  document.getElementById("survey-popup").classList.remove("flex");
+  document.getElementById("survey-popup").classList.remove("justify-center");
+// remove from body the class of "overflow-hidden" to allow scrolling when the pop-up window is closed
+  document.querySelector("body").classList.remove("overflow-hidden");
+  document.querySelector("header").classList.remove("hidden");
 }
