4a9011df970b5262a29dafa2c42f9811f8924f43
upgradedtofml <ckan1995@hotmail.com>
2023-01-24 18:24:37 +0200

    small changes

--- profile.php
+++ profile.php
@@ -1,7 +1,12 @@
 
 <?php
+
 session_start();
-if($_SERVER["REQUEST_METHOD"] == "POST") {
+$id= $_SESSION["id"] ;
+$username = $_SESSION["username"];
+     function fuckMyexistence(){
+if($_SERVER["REQUEST_METHOD"] == "POST") 
+{
 
     $name = htmlspecialchars($_POST["name"], ENT_QUOTES, 'UTF-8');
     $description = htmlspecialchars($_POST["description"], ENT_QUOTES, 'UTF-8');
@@ -9,14 +14,15 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
     include "profile/profileinfo.php";
     include "profile/profilecontr.php";
     include "profile/profileview.php";
-
-    $username = $_SESSION["username"];
-    $id = $_SESSION["id"];
+    
     $profileInfo = new ProfileInfoController($username,$id);
     $placeholder = "this is a text";
-    $profileInfo->updateProfileInfo($description, $name,$placeholder );
-    header('location:public\Profile.php');
+    $profileInfo->updateProfileInfo($description, $name,$placeholder);
+    
 
     
-} 
+}
+}
+
+
 ?>
