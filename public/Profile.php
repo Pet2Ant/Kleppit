3a4993169c237bec15a07fe959982ecfab0386f7
Aurora Takemi <98479040+auroraisluna@users.noreply.github.com>
2023-01-28 23:33:19 +0200

    CONVERT SEARCH TO ELEMENT

--- public/Profile.php
+++ public/Profile.php
@@ -7,6 +7,7 @@ include "../profile/profileview.php";
 include "../post/postsql.php";
 include "../post/postcont.php";
 include "../post/postview.php";
+include '../search/sharebar.php';
 $profileInfo = new ProfileInfoView();
 $postInfo = new PostInfoView();
 
@@ -78,12 +79,10 @@ Post content text: text-gray-500
                     Post
                 </button>
                 <!-- Search bar -->
-                <div class="mx-4 flex flex-1 items-center space-x-3 rounded border border-[#343536] bg-[#272729] px-4 py-1.5">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true" class="h-5 w-5 text-[#878A8C]">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
-                    </svg>
-                    <input class="flex-1 bg-transparent text-sm focus:outline-none text-gray-400" type="text" placeholder="Search Kleppit" />
-                </div>
+                <?php
+                    $searchbar = new Searchbar();
+                    $searchbar->generateSearchbar();
+                ?>
             </div>
             <!-- User button -->
             <div class="space-x-4 py-6 flex flex-col justify-center ">
