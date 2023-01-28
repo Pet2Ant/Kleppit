bb707d9d50408a6d985ade4de26f43951e415fc4
Aurora Takemi <98479040+auroraisluna@users.noreply.github.com>
2023-01-28 23:34:46 +0200

    cringe text removal

--- search/sharebar.php
+++ search/sharebar.php
@@ -8,7 +8,7 @@ class Searchbar {
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" aria-hidden="true" class="h-5 w-5 text-[#878A8C]">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
             </svg>
-            <input class="flex-1 bg-transparent text-sm focus:outline-none text-gray-400" type="text" placeholder="Search Kleppit OWO OWO" />
+            <input class="flex-1 bg-transparent text-sm focus:outline-none text-gray-400" type="text" placeholder="Search Kleppit" />
         </div>';
     }
 
